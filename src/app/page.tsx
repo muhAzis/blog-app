41e79c65691ec4b836ad8e4f3a066e44cf3bd7df
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-04-20 08:10:38 +0700

    + Initial commit, adding default app resources

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import '@/styles/views/HomePage.scss';
+import Posts from '@/views/Posts';
+
+export default function Home() {
+  return (
+    <main id="homePage">
+      <Posts />
+    </main>
+  );
+}
