ea76add1e76f8a5e14dfa746e2adf1f99ed94a0b
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-04-21 23:22:07 +0700

    - Adding Bootstrap Icon url dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
