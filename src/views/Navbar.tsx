0115c3c104ac405ca7d3bad844438597fd9183a2
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-04-24 07:43:11 +0700

    - Adding back button when entering user's profile page

--- src/views/Navbar.tsx
+++ src/views/Navbar.tsx
@@ -1,11 +1,29 @@
+'use client';
 import React from 'react';
 import '@/styles/views/Navbar.scss';
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav id="navbar">
       <ul className="menu-links">
+        {pathname.includes('users/') && (
+          <li
+            className="menu-link"
+            style={{
+              position: 'absolute',
+              width: '100px',
+              left: '2rem',
+            }}
+          >
+            <Link href="/users" className="item">
+              <i className="bi bi-arrow-left" />
+            </Link>
+          </li>
+        )}
         <li className="menu-link">
           <Link href="/" className="item">
             <i className="bi bi-house-door-fill" />
